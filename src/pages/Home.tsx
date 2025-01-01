4f132a69d51bbf619e10e0dfd1d0fde35832836b
Md Adnan Baqi <adnan.baki786@gmail.com>
2025-01-02 03:05:57 +0530

    Revert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import { Hero } from '@/components/Hero';
 import { Features } from '@/components/Features';
-import { TrustedByBanner } from '@/components/TrustedByBanner';
 
 export function Home() {
   return (
     <div>
       <Hero />
-      <TrustedByBanner/>
       <Features />
     </div>
   );
